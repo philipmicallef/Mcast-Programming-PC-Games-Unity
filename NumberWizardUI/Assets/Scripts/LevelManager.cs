64da2722444e0eb14d46003c4822705e62b6131d
philipmicallef <32672929+philipmicallef@users.noreply.github.com>
2017-10-19 09:37:38 +0200

    NumberWizardUI

---
+++ NumberWizardUI/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+
+	public void Quit()
+	{
+		UnityEditor.EditorApplication.isPlaying = false;	
+	}
+
+
+
+
+
+
+	public void Loadlevel(string levelName)
+	{
+		print ("Loading level" + levelName);
+		// loads the Scene named levelName
+
+		SceneManager.LoadScene(levelName);
+	}
+
+}
