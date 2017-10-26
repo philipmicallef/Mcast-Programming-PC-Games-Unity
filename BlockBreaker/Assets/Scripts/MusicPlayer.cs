0bbb44fbb771c260cb0cf50bbbd23add2d9b3392
philipmicallef <32672929+philipmicallef@users.noreply.github.com>
2017-10-26 09:42:26 +0200

    BlockBreaker

---
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer myMusicPlayer = null;
+
+
+	void Awake() // the first method that is run in Unity
+	{
+		// if myMusicPlayer is the 1st instance
+		if (myMusicPlayer == null) 
+		{
+			//myMusicPlayer = 1st SoundObject
+			myMusicPlayer = this;
+
+			//this.gameObject = the gameObject attached to this script, in our case its SoundObject
+			//DontDestroyOnLoad = do not destroy the gameObject when changing scenes
+			GameObject.DontDestroyOnLoad (this.gameObject);
+		}
+
+		//if myMusicPlayer is NOT null
+		// if a SoundObject already exists
+		else
+		{
+			print ("Sound object" + this.gameObject.GetInstanceID ().ToString ());
+			print("First Sound Object");
+
+			//Destroy the 2nd gameObject created
+			Destroy (this.gameObject);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
